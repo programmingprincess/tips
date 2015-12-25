4413e9aa746684e7fcb243efccc3ec231e5ce636
Jiaqi Wu <jiaqiwu@asu.edu>
2015-12-25 15:12:18 -0700

    first commit

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*All outlets go here*/
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    /*End of outlets*/
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +35,29 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.20, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        /*Two decimal places*/
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
 }
 
